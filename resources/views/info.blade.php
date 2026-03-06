3b11b4910d744805eb08ef282432553163e09497
emils <emils.rozentaals@gmail.com>
2026-03-06 12:57:57 +0200

    Added a new info route with lorem25

---
+++ resources/views/info.blade.php
@@ -0,0 +1,1 @@
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam asperiores quasi accusamus dolores nostrum non amet consequatur quas neque, excepturi tempore ea adipisci laudantium illum.</p>
